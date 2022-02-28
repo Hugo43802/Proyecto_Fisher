078d989c7ce995f008401325733e799d501e3dca
Hugo Vega <hugo43802@gmail.com>
2022-02-27 20:09:16 -0500

    Limpieza y mejora motor MA4

--- PROGRAMAS/Proceso_Fisher_CIM/Despacho/Despacho.py
+++ PROGRAMAS/Proceso_Fisher_CIM/Despacho/Despacho.py
@@ -10,6 +10,7 @@ import ftrobopy
 
 plc = ftrobopy.ftrobopy("192.168.0.101")
 
+
 def motor_MA4():
     MA4 = plc.output(5)
     MA4.setLevel(512)
@@ -17,15 +18,14 @@ def motor_MA4():
     MA4.setLevel(0)
             
 def banda1():
+    # Permiso me sirve para verificar que la función motor_MA4 solo se ejecute una vez
+    permiso = 1
     # se crea el objeto sensor reed
     b1 = plc.input(2)
-    b1_estado = b1.state()
     
     b2 = plc.input(3)
-    b2_estado = b2.state()
     
     b3 = plc.input(4)
-    b3_estado = b3.state()
     
     ''' creación de objeto salida
         En este caso los motores se manejan como salidas 
@@ -41,8 +41,6 @@ def banda1():
     
     while True:
         cambio = 1
-        #print("El estado de B1 es = ", b1_estado)
-        #print("El estado de B2 es = ", b2_estado)
         ns_b1 = b1.state()
         ns_b2 = b2.state()
         ns_b3 = b3.state()
@@ -50,14 +48,13 @@ def banda1():
         if cambio == 1 and ns_b1 != 0:
             MA5.setLevel(512)
             MA3.setLevel(512)
-            #print(" B1 Estado nuevo: ", ns_b1)
         
-        if ns_b2 != 0:
+        if ns_b2 != 0 and permiso == 1:
             cambio = 0
             MA3.setLevel(0)
             MA5.setLevel(0)
-            #print(" B2 Estado nuevo: ", ns_b2)
-            motor_MA4()         
+            motor_MA4()  
+            permiso= 0       
             
             '''
                 Espera  segundo para encender nuevamente los motores
@@ -67,12 +64,10 @@ def banda1():
             
             MA3.setLevel(512)
             MA2.setLevel(512)
-            
                        
         if ns_b3 != 0:
             MA3.setLevel(0)
             MA2.setLevel(0)
-                             
          
         plc.updateWait()
               
