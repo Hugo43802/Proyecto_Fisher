0e64525584932c93977f65d5d7437ab4dda0b67b
Hugo Vega <hugo4802@gmail.com>
2022-03-03 10:33:14 -0500

    archivo rampas funcionando

--- PROGRAMAS/Proceso_Fisher_CIM/Despacho/Rampas.py
+++ PROGRAMAS/Proceso_Fisher_CIM/Despacho/Rampas.py
@@ -1,30 +1,22 @@
-'''
-    Este programa genera el movimiento del proceso de 
-    Despacho
-'''
 from time import sleep
+from Despacho import *
 import ftrobopy
 
-# función para conectar el plc
 
 plc = ftrobopy.ftrobopy("192.168.0.101")
 
-
 def motor_MA4():
-    
     MA4 = plc.output(5)
     MA4.setLevel(512)
     sleep(2)
     MA4.setLevel(0)
-            
+    
 def banda1():
     # Permiso me sirve para verificar que la función motor_MA4 solo se ejecute una vez
     permiso = 1
     # se crea el objeto sensor reed
     b1 = plc.input(2)
-    
     b2 = plc.input(3)
-    
     b3 = plc.input(4)
     
     ''' creación de objeto salida
@@ -39,7 +31,9 @@ def banda1():
     #print("El estado del sensor B1 es: ", b1_estado)
     #print("El estado del sensor B2 es: ", b2_estado)
     
-    while True:
+    cambio_while = True
+    
+    while cambio_while:
         cambio = 1
         ns_b1 = b1.state()
         ns_b2 = b2.state()
@@ -69,8 +63,48 @@ def banda1():
         if ns_b3 != 0:
             MA3.setLevel(0)
             MA2.setLevel(0)
+            
+        cambio_while = False
+        break
          
+        
+    
+def run():
+    B4 = plc.input(5)
+    B5 = plc.input(6)
+    B6 = plc.input(7)
+    B7 = plc.input(8)
+    
+    MA2 = plc.output(6)
+    
+    while True:
+        #Variable para verificar el cambio
+        rampa = 0
+        
+        estado_B4 = B4.state()
+        estado_B5 = B5.state()
+        estado_B6 = B6.state()
+        estado_B7 = B7.state()
+        
+        if estado_B4 != 1 and rampa == 0:
+            rampa = 1
+            print("El producto va a la rampa # 1")
+            banda1()
+        elif estado_B5 != 1:
+            rampa = 1
+            print("El producto va a la rampa # 2")
+            banda1()
+        elif estado_B6 != 1:
+            print("El producto va a la rampa # 3")
+            banda1()
+        elif estado_B7 != 1:
+            print("El producto va a la rampa # 4")
+            banda1()
+        else:
+            print("Todas las rampas llenas")
+        
         plc.updateWait()
-              
+
+
 if __name__ == "__main__":
-    banda1()
+    run()
