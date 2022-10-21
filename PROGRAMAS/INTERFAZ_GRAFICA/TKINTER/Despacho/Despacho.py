db75c201c454d0ba5e732a78aeec40d27de5b38d
Hugo Vega <hugo4802@gmail.com>
2022-10-21 13:49:37 -0500

    Modificacion m.py Para conectar con Interfaz

---
+++ PROGRAMAS/INTERFAZ_GRAFICA/TKINTER/Despacho/Despacho.py
@@ -0,0 +1,295 @@
+import time 
+import ftrobopy
+import random
+from m import *
+from tkinter import *
+
+
+'''
+    VARIABLES
+'''  
+# Crear diccionario de colores
+
+colores = {
+    "Azul": 4,
+    "Rojo": 7,
+    "Blanco": 10,
+}
+
+# Se crean los arreglos vacíos para guardar tiempos y claves
+salida_Tiempos = []
+salida_Claves = []
+
+'''
+    INICIO DE PLC
+'''
+plc = ftrobopy.ftrobopy("192.168.0.117") # ip de la U
+
+
+# Inicialización de variables y objetos
+
+## SENSORES REED
+B1 = plc.input(2)
+B2 = plc.input(3)
+B3 = plc.input(4)
+
+B4 = plc.input(5) #cambiar a 5, 6, 7 y 8 para probar en original
+B5 = plc.input(6)
+B6 = plc.input(7)
+B7 = plc.input(8)
+
+### MOTORES
+#Reset y banda lineal
+MA1 = plc.output(1)
+MA1_Reverso = plc.output(2)
+#Despacho
+MA5 = plc.output(3)
+MA3 = plc.output(4) #Cambiar a 4 y 6 para probar original1
+MA2 = plc.output(6)
+
+#### BOTÓN 
+BG1 = plc.input(1)
+
+def aleatorio(msj_Fichas, msj_Sumatxt):
+    '''
+        Esta función elige entre diferentes combinaciones de colores
+        para dar las familias de producto que serán procesadas 
+        por la función vinipelado y eje_lineal.
+        
+        Esta función retorna la sumatoria de tiempos de los colores
+        y lo divide a la mitad.
+    '''
+    
+    #msj_Fichas.set(salida_Claves)
+    #msj_Sumatxt.set(suma)
+    
+    for elem in range(4):
+        ## Elección de valores al azar
+        alea = random.choice(list(colores.values()))
+
+        ## pos = Guarda los elementos mutables en lista (valores)
+        pos = list(colores.values())
+        ## llaves = Guarda las claves mutables en lista (claves)
+        llaves = list(colores.keys())
+
+        ## llave obtiene los valores y reconoce las claves
+        llave = pos.index(alea)
+
+        # Agregar a la lista salida tiempos, los valores
+        salida_Tiempos.append(alea)
+        # Agregar para visualizar los colores seleccionados.
+        salida_Claves.append(llaves[llave])
+
+        #Sumar los valores del arreglo tiempos
+        suma = sum(salida_Tiempos)
+        
+        print(salida_Tiempos)
+        print(salida_Claves)
+        print("La suma de los tiempos es: ", suma)
+
+        msj_Fichas.set(salida_Claves)
+        msj_Sumatxt.set(suma)        
+    return suma
+  
+
+## Función Reset para verificar que el motor MA1 siempre esté en la posición de 
+# origen
+def reset():
+    start_time = time.time()
+    '''
+        Función que permite enviar el eje lineal de nuevo a su posición original
+    '''
+    while BG1.state() != 0:
+        MA1.setLevel(0)
+        print("Eje en el origen")
+        break
+    else:
+        MA1.setLevel(512)
+
+    #tf= time.time()
+    #print("Tiempo de eje", tf-start_time) 
+
+
+def vinipelado(msj_Fichas, msj_Sumatxt):
+    start_time = time.time()
+    '''
+        Función que activa el motor MA4 del prodceso de despacho,
+        adicional se obtiene el tiempo de la combinación de colores
+        obtenida de la función aleatorio.
+    '''
+    
+    tiempo = aleatorio(msj_Fichas, msj_Sumatxt)/3
+    MA4 = plc.output(5) # Para probar en el sistema orginal cambiar a 5
+    time.sleep(tiempo)
+    MA4.setLevel(512)
+    time.sleep(tiempo)
+    #print("El tiempo de vinipelado es: ",tiempo)
+    MA4.setLevel(0)
+    
+    tf= time.time()
+    print("Tiempo de vinipelado", tf-start_time) 
+
+def eje_lineal(num_Rampa,sensor, pulsos, tiempo):
+    start_time = time.time()
+    '''
+        num_Rampa = Número de la rampa hacia la que se dirigirá el motor MA1_Reverso
+
+        sensor = Objeto sensor que se va a actualizar. Se recibe de la función despacho.
+        
+        pulsos = Cantidad de pasos que debe ejercer el motor para llegar a la meta
+        Esta función genera el movimiento del eje lineal para transportar según la 
+        cantidad de pulsos el producto que se encuentra en la banda.
+        
+        tiempo = Suma de los tiempos obtenidos de la función aleatorio().
+
+        La función eje_lineal genera el movimiento del motor MA1 y MA1_Reverso, para ubicar
+        la banda en las diferentes rampas según los pulsos y se detiene al identificar el 
+        sensor elegido para cada rampa.
+        
+    '''
+
+    #get.CurrentCounterValue lee el contador existente en la conexión.
+    #pos_inicial_eje = posición inicial del motor frente a la rampa 1
+
+    #meta = Variable a la que se quiere llegar (objetivo), es la posición de la banda para llegar a las rampas
+    #(Revisar archivo Rampas.txt)
+
+    # C1 = Variable que guarda los datos del contador desde el PLC
+
+    # Los estados B4, B5, B6 y B7, verifican si el producto pasa por el sensor
+    time.sleep(tiempo)
+    
+    pos_inicial_eje = plc.getCurrentCounterValue(0)
+    #print("El valor de C2 es: ", pos_inicial_eje)
+
+    meta = pos_inicial_eje + pulsos
+
+    cambio_while = True
+
+    while cambio_while:
+        MA1.setLevel(0)
+        
+        C1 = plc.getCurrentCounterValue(0) 
+        #print(C1)
+        
+        #print("La meta es: ", meta)
+        #print("Los pulsos contados son: ",pulsos)
+        
+        MA1_Reverso.setLevel(512)
+        
+        while (plc.getCurrentCounterValue(0) >= meta) and (cambio_while == True): #Para no usar break, simplemente se mantiene el cambio a True
+            estado = sensor.state() # el estado será el cambio del objeto dependiendo de la rampa
+            MA1_Reverso.setLevel(0)
+            #print("Los pulsos contados son: ",pulsos)
+            #print("Eje en posición")
+            # sleep(2)
+            
+            MA2.setLevel(512)
+            # sleep(2)
+            #print("La rampa es: ", num_Rampa)
+            
+            if estado == 1: 
+                MA2.setLevel(0)
+                print(f"Rampa #{num_Rampa} lista")
+                cambio_while =False
+                
+    time.sleep(tiempo)
+    reset()
+    time.sleep(4)
+    
+    tf= time.time()
+    print("Tiempo de eje_lineal", tf-start_time)
+    
+    rampas()
+        
+    plc.updateWait()
+
+def despacho(num_Rampa,sensor,pulsos,msj_Fichas, msj_Sumatxt):
+    start_time = time.time()
+    '''
+        num_Rampa: Número de la rampa a la que el producto se dirigirá
+
+        sensor: Número del sensor recibido de la función rampas()
+
+        pulsos: Es la cantidad de pasos que debe dar el motor para llegar a cada rampa
+    '''
+    reset()
+    
+    cambio_while = True
+
+    while cambio_while:
+        cambio = 1
+
+        # Se reconoce en qué estado se encuentran los sensores 1 = activado o 0 = desactivado
+        estado_b1 = B1.state()
+        estado_b2 = B2.state()
+        estado_b3 = B3.state()
+
+        if cambio == 1 and estado_b1 != 0:
+            MA5.setLevel(512)
+            MA3.setLevel(500)
+        
+        if estado_b2 != 0:
+            cambio = 0
+            MA3.setLevel(0)
+            MA5.setLevel(0)
+
+            vinipelado(msj_Fichas, msj_Sumatxt)
+            
+            #sleep(0.5)
+
+            MA3.setLevel(500)
+            MA2.setLevel(512)
+        
+        if estado_b3 != 0:
+            MA3.setLevel(0)
+            MA2.setLevel(0)
+            
+            tiempo = aleatorio(msj_Fichas, msj_Sumatxt)/2
+            tf= time.time()
+            print("Tiempo de despacho: ", tf-start_time) 
+            eje_lineal(num_Rampa, sensor, pulsos, tiempo) #Envia los datos directamente a la función eje lineal con el número del sensor
+    
+    cambio_while = False
+    
+    plc.updateWait()
+
+def rampas(msj_Fichas, msj_Sumatxt):
+    start_time = time.time()
+    
+    cambio_while = True
+    while cambio_while:
+
+        rampa = 0
+
+        estado_B4 = B4.state()
+        estado_B5 = B5.state()
+        estado_B6 = B6.state()
+        estado_B7 = B7.state()
+
+        if estado_B4 != 1:
+            rampa = 1
+            print("¡El producto se dirige a la rampa #1!")
+            despacho(rampa,B4,0,msj_Fichas, msj_Sumatxt)
+        elif estado_B5 != 1:
+            rampa = 2
+            print("¡El producto se dirige a la rampa #2!")
+            despacho(rampa,B5,190,msj_Fichas, msj_Sumatxt)
+        elif estado_B6 != 1:
+            rampa = 3
+            print("¡El producto se dirige a la rampa #3!")
+            despacho(rampa,B6 ,415,msj_Fichas, msj_Sumatxt)
+        elif estado_B7 != 1:
+            rampa = 4
+            print("¡El producto se dirige a la rampa #4!")
+            despacho(rampa,B7, 627,msj_Fichas, msj_Sumatxt)
+        else:
+            print("¡Todas las rampas están llenas!")
+
+        tf= time.time()
+        
+        plc.updateWait()
+    print("Tiempo de rampas: ", tf-start_time) 
+    
+''' if __name__ == "__main__":
+    rampas() '''
---
+++ PROGRAMAS/INTERFAZ_GRAFICA/TKINTER/Despacho/m.py
@@ -0,0 +1,164 @@
+from tkinter import *
+from Despacho import *
+
+class Automatico:
+    def __init__(self):
+        self.ventana_auto = Tk()
+        self.ventana_auto.geometry("400x640")
+        self.ventana_auto.resizable(0,0)
+        self.ventana_auto.title("Automático")
+        self.ventana_auto.eval('tk::PlaceWindow . center')
+
+        # configurar el grid
+        self.ventana_auto.columnconfigure(0, weight=1)
+        self.ventana_auto.columnconfigure(1, weight=1)
+        self.ventana_auto.columnconfigure(2, weight=1)
+        self.ventana_auto.columnconfigure(3, weight=1)
+        self.ventana_auto.columnconfigure(4, weight=1)
+
+        self.msj_opciones = Label(
+            text="Combinación de Productos",
+            font= ('Helvetica', 10, 'bold')
+        ).grid(row=0, column=0, columnspan=5)
+    
+    # ALEATORIO    
+        fichas = StringVar()
+        self.msj_Fichas = Label(
+            width=30,
+            height=5,
+            textvariable=fichas,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        )
+        self.msj_Fichas.grid(row=1, column=0, columnspan=5)
+        # tx.set("Hola Mundo")
+
+
+        self.msj_Sumalbl = Label(
+            text="Sumatoria de Tiempos:",
+            width=20
+        ).grid(row=2, column=1, pady=10)
+
+        sum_Fichas = StringVar()
+        self.msj_Sumatxt = Label(
+            width=8,
+            height=1,
+            relief="groove",
+            borderwidth=4,
+            textvariable=sum_Fichas,
+            bg="white"
+        ).grid(row=2, column=2)
+        
+       
+
+# PROCESO
+
+        self.msj_Proceso = Label(
+            text="Proceso de Despacho",
+            font= ('Helvetica', 10, 'bold')
+        ).grid(row=3, column=0, pady=5, columnspan=5)
+
+        self.msj_Despacho_ini = Label(
+            width=30,
+            height=5,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=4, column=0, columnspan=5)
+
+        self.msj_Banda = Label(
+            width=30,
+            height=1,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=5, column=0, columnspan=5, pady=10)
+
+# VINIPELADO
+
+        self.msj_Vinipelado = Label(
+            text="Tiempo de Vinipelado:",
+            width=20
+        ).grid(row=6, column=1, pady=5)
+
+        self.msj_VinipeladoTxt = Label(
+            width=8,
+            height=1,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=6, column=2)
+
+# EJE LINEAL
+        self.msj_Eje = Label(
+            text="Eje Lineal",
+            font= ('Helvetica', 10, 'bold')
+        ).grid(row=7, column=0, columnspan= 5, pady=10)
+
+        self.ms_Eje_Estado = Label(
+            width=30,
+            height=1,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=8, column=0, columnspan=5)
+
+        self.msj_Eje_Espera = Label(
+            text="Tiempo de Espera:",
+            width=20
+        ).grid(row=9, column=1, pady=5)
+
+        self.msj_EjeTxt = Label(
+            width=8,
+            height=1,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=9, column=2)
+
+# ENTREGA
+        self.msj_Entrega = Label(
+            text="Entrega",
+            font= ('Helvetica', 10, 'bold')
+        ).grid(row=10, column=0, columnspan= 5, pady=10)
+
+        self.msj_Entrega_Banda = Label(
+            width=30,
+            height=1,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=11, column=0, columnspan= 5, pady=5)
+
+        self.msj_NextTarea = Label(
+            width=30,
+            height=5,
+            relief="groove",
+            borderwidth=4,
+            bg="white"
+        ).grid(row=12, column=0, columnspan=5)
+        
+        #Botón de inicio
+        def btn_Inicio():
+            aleatorio(fichas, sum_Fichas)
+            
+            #rampas(fichas, sum_Fichas)   
+            
+        self.Inicio = Button(
+            text= "Inicio",
+            width=10,
+            height=1,
+            relief="solid",
+            command= btn_Inicio
+        ).grid(row=13, column=0, columnspan=5, pady=5)
+        
+        
+               
+        self.ventana_auto.mainloop()
+        # No permite que se hagan acciones en otras ventanas 
+                
+if __name__ == "__main__":
+        app = Automatico()
+
+
